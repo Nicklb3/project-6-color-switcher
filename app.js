78ca5fe497b222c51f43beec4630cd9cfdf45a2a
Nicholas Bonner <nicholasbonner@Nicholass-Mac-mini.local>
2023-07-13 01:42:56 -0700

    finised fuctioning

---
+++ app.js
@@ -0,0 +1,21 @@
+const body = document.body
+const switchBtn = document.querySelector('.switch');
+const switchPara = document.querySelector('.color');
+
+switchBtn.addEventListener('click', function () {
+   let color1 = getRandomNum();
+   let color2 = getRandomNum();
+   let color3 = getRandomNum();
+
+   const colorString = `rgb(${color1}, ${color2}, ${color3})`;
+//    console.log(colorString);
+
+body.style.backgroundColor = colorString;
+switchPara.innerText = colorString;
+});
+
+function getRandomNum () {
+    return Math.floor(Math.random() * 256);
+}
+
+// try with # color code
